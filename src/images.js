8bed0184e43f3a35f33a57f03b387cb8307300f0
tiago <70700766+tiagorangel1@users.noreply.github.com>
2025-07-21 17:00:12 +0100

    use cloudflare proxy

--- src/images.js
+++ src/images.js
@@ -1,3 +1,16 @@
+import { socksDispatcher } from "fetch-socks";
+
+// prevents the ip from showing up when we hit a ratelimit.
+
+// make sure a cloudflare warp proxy is running.
+// i found https://blog.caomingjun.com/run-cloudflare-warp-in-docker/en/
+// with WARP_SLEEP=6 to be pretty good.
+const dispatcher = socksDispatcher({
+  type: 5,
+  host: "::1",
+  port: 1080,
+});
+
 export default async function genImage(prompt) {
   const width = 1024;
   const height = 1024;
@@ -7,6 +20,7 @@ export default async function genImage(prompt) {
 
   const response = await fetch(url, {
     method: "GET",
+    dispatcher
   });
 
   const arrayBuffer = await response.arrayBuffer();
